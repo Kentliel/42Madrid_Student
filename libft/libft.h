84b2c48ccbe0e0dc989e1d18c9cac78507cd0cfc
Kent <kcarrero@localhost.localdomain>
2025-04-15 13:47:21 +0200

    funcion strlcat y toupper

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: kcarrero <kcarrero@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 17:04:06 by kcarrero          #+#    #+#             */
-/*   Updated: 2025/04/11 16:19:04 by kcarrero         ###   ########.fr       */
+/*   Updated: 2025/04/15 13:45:54 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int n);
 
 #endif
