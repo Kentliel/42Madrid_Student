1633c99faf648cdf53ae27f5f765b41a94ce6759
Kent <kcarrero@localhost.localdomain>
2025-04-20 23:03:47 +0200

    funcion strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: kcarrero <kcarrero@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 21:06:15 by kcarrero          #+#    #+#             */
-/*   Updated: 2025/04/16 21:50:21 by kcarrero         ###   ########.fr       */
+/*   Updated: 2025/04/20 22:56:09 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,11 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		return ((char *)haystack);
 	while (haystack[i] && i + needle_len <= len)
 	{
-
+		while (needle[j] && haystack[i + j] == needle[j])
+			j++;
+		if (j == needle_len)
+			return ((char *)&haystack[i]);
+		i++;
 	}
+	return (NULL);
 }
