bf69b0def114005926b6b0cec4a6a7944ab16535
Kent <kcarrero@localhost.localdomain>
2025-04-27 22:49:30 +0200

    funcion strmapi

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: kcarrero <kcarrero@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 17:04:06 by kcarrero          #+#    #+#             */
-/*   Updated: 2025/04/27 22:33:03 by kcarrero         ###   ########.fr       */
+/*   Updated: 2025/04/27 22:46:33 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
