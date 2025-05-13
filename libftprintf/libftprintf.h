ea511dd3b7ad3668d73a71438466fe7e75a7db1f
Kent Carrero <kcarrero@c2r12s4.42madrid.com>
2025-05-13 20:08:52 +0200

    comienzo del printf

---
+++ libftprintf/libftprintf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcarrero <kcarrero@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 19:28:06 by kcarrero          #+#    #+#             */
+/*   Updated: 2025/05/13 19:46:33 by kcarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+#include "libft/libft.h"
+#include <stdarg.h>
+
+
+
+
+#endif // LIBFTPRINTF_H
