5d4c4de32022aec57476f90a5c0ca1c6c3e3d25f
Kent Carrero <kcarrero@c2r7s2.42madrid.com>
2025-06-05 12:12:47 +0200

    get_next_line comming

--- libftprintf/main.c
+++ libftprintf/main.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/libftprintf.h"
+#include "includes/ft_printf.h"
 #include <stdio.h>
 #include <limits.h>
 
