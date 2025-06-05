a92bb264799c9366e638fa0762dd0cb12caec00b
kcarrero-porsche <kent.iluminati@gmail.com>
2025-06-05 17:46:24 +0200

    creacion de libreria de get next line

--- get_next_line/includes/get_next_line.h
+++ get_next_line/includes/get_next_line.h
@@ -6,8 +6,24 @@
 /*   By: kcarrero <kcarrero@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 12:23:06 by kcarrero          #+#    #+#             */
-/*   Updated: 2025/05/29 12:23:27 by kcarrero         ###   ########.fr       */
+/*   Updated: 2025/06/05 17:19:59 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin_gnl(char *s1, char *s2);
+char	*ft_strdup_gnl(const char *s);
+char	*ft_strchr_gnl(const char *s, int c);
+size_t	ft_strlen_gnl(const char *s);
+
+#endif
