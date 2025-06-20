817c85155a0d2c6c3d4427d0581cd481b54280cd
Kent Carrero <kcarrero@c1r11s3.42madrid.com>
2025-06-20 19:40:11 +0200

    bonus hecho

---
+++ get_next_line/bonus/get_next_line_bonus.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcarrero <kcarrero@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 12:19:27 by kcarrero          #+#    #+#             */
+/*   Updated: 2025/06/20 19:26:15 by kcarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*ft_read_and_update_cache(int fd, char *cache, int *finished)
+{
+	char	*buffer;
+	ssize_t	bytes_read;
+	char	*tmp;
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0)
+		return (free(buffer), free(cache), NULL);
+	buffer[bytes_read] = '\0';
+	if (bytes_read == 0)
+		return (free(buffer), *finished = 1, cache);
+	if (!cache)
+		cache = ft_strdup_gnl(buffer);
+	else
+	{
+		tmp = ft_strjoin_gnl(cache, buffer);
+		free(cache);
+		if (!tmp)
+			return (free(buffer), NULL);
+		cache = tmp;
+	}
+	return (free(buffer), cache);
+}
+
+static char	*ft_read_and_cache(int fd, char *cache)
+{
+	int	finished;
+
+	finished = 0;
+	while (!ft_strchr_gnl(cache, '\n') && !finished)
+	{
+		cache = ft_read_and_update_cache(fd, cache, &finished);
+		if (!cache)
+			return (NULL);
+		if (ft_strlen_gnl(cache) == 0)
+			return (free(cache), NULL);
+	}
+	return (cache);
+}
+
+static char	*ft_extract_line(char *cache)
+{
+	int		i;
+	char	*line;
+
+	if (!cache || !cache[0])
+		return (NULL);
+	i = 0;
+	while (cache[i] && cache[i] != '\n')
+		i++;
+	if (cache[i] == '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	ft_memcpy_gnl(line, cache, i);
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*ft_update_cache(char *cache)
+{
+	char	*new_cache;
+	int		i;
+	int		cache_len;
+
+	if (!cache)
+		return (NULL);
+	i = 0;
+	while (cache[i] && cache[i] != '\n')
+		i++;
+	if (!cache[i])
+		return (free(cache), NULL);
+	i++;
+	cache_len = ft_strlen_gnl(cache);
+	new_cache = (char *)malloc(sizeof(char) * (cache_len - i + 1));
+	if (!new_cache)
+		return (NULL);
+	ft_memcpy_gnl(new_cache, cache + i, cache_len - i);
+	new_cache[cache_len - i] = '\0';
+	free(cache);
+	return (new_cache);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*cache[1024];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 1024)
+		return (NULL);
+	cache[fd] = ft_read_and_cache(fd, cache[fd]);
+	if (!cache[fd])
+		return (NULL);
+	line = ft_extract_line(cache[fd]);
+	cache[fd] = ft_update_cache(cache[fd]);
+	return (line);
+}
---
+++ get_next_line/bonus/get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcarrero <kcarrero@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 12:23:06 by kcarrero          #+#    #+#             */
+/*   Updated: 2025/06/20 19:26:54 by kcarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin_gnl(char const *s1, char const *s2);
+char	*ft_strdup_gnl(const char *s);
+char	*ft_strchr_gnl(const char *s, int c);
+void	*ft_memcpy_gnl(void *dest, const void *src, size_t n);
+size_t	ft_strlen_gnl(const char *s);
+
+#endif
