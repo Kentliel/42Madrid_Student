ad3b2219e0a92dfbb6d5d6f000dfe3897f04f55e
Kentliel <kent.iluminati@gmail.com>
2026-01-14 22:11:53 +0100

    mejora de error de cierre temprano de los hijos

--- pipex/srcs/child_process.c
+++ pipex/srcs/child_process.c
@@ -6,20 +6,47 @@
 /*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 18:54:55 by kcarrero          #+#    #+#             */
-/*   Updated: 2026/01/14 20:51:38 by kcarrero         ###   ########.fr       */
+/*   Updated: 2026/01/14 22:09:14 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static int	redirect_io(t_pipex *px)
+{
+	int	tmp;
+
+	if (px->in_fd == -1)
+	{
+		tmp = open("/dev/null", O_RDONLY);
+		if (tmp == -1)
+		{
+			perror("open /dev/null");
+			return (-1);
+		}
+		px->in_fd = tmp;
+	}
+	if (dup2(px->in_fd, STDIN_FILENO) == -1)
+	{
+		perror("dup2 stdin");
+		return (-1);
+	}
+	if (dup2(px->out_fd, STDOUT_FILENO) == -1)
+	{
+		perror("dup2 stdout");
+		return (-1);
+	}
+	close(px->in_fd);
+	close(px->out_fd);
+	return (0);
+}
 
 void	execute_child(char *cmd, t_pipex *px)
 {
 	char	**args;
 	char	*exec;
 
-	if (dup2(px->in_fd, STDIN_FILENO) == -1)
-		exit(1);
-	if (dup2(px->out_fd, STDOUT_FILENO) == -1)
+	if (redirect_io(px) == -1)
 		exit(1);
 	args = parse_command(cmd);
 	exec = find_executable(args[0], px->envp);
@@ -41,9 +68,17 @@ pid_t	create_child(char *cmd, t_pipex *px)
 	pid_t	pid;
 
 	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (-1);
+	}
 	if (pid == 0)
 	{
-		close(px->close_fd);
+		if (px->in_fd == px->pipe_fd[0])
+			close(px->pipe_fd[1]);
+		else if (px->out_fd == px->pipe_fd[1])
+			close(px->pipe_fd[0]);
 		execute_child(cmd, px);
 	}
 	return (pid);
--- pipex/srcs/main.c
+++ pipex/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 19:10:41 by kcarrero          #+#    #+#             */
-/*   Updated: 2026/01/14 20:51:07 by kcarrero         ###   ########.fr       */
+/*   Updated: 2026/01/14 22:07:46 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ static void	open_files(char **argv, t_pipex *px)
 {
 	px->in_fd = open(argv[1], O_RDONLY);
 	if (px->in_fd == -1)
-	{
 		perror(argv[1]);
-		exit(1);
-	}
 	px->out_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (px->out_fd == -1)
 	{
@@ -41,21 +38,22 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	px;
 	int		status;
+	int		outfile_fd;
 
 	if (argc != 5)
 		return (ft_printf("Usage: ./pipex file1 cmd1 cmd2 file2\n"), 1);
 	px.envp = envp;
 	open_files(argv, &px);
+	outfile_fd = px.out_fd;
 	if (pipe(px.pipe_fd) == -1)
 		return (perror("pipe"), 1);
-	px.close_fd = px.pipe_fd[0];
 	px.pid1 = launch(argv[2], &px, px.in_fd, px.pipe_fd[1]);
-	px.close_fd = px.pipe_fd[1];
-	px.pid2 = launch(argv[3], &px, px.pipe_fd[0], px.out_fd);
-	close(px.in_fd);
-	close(px.out_fd);
+	px.pid2 = launch(argv[3], &px, px.pipe_fd[0], outfile_fd);
 	close(px.pipe_fd[0]);
 	close(px.pipe_fd[1]);
+	if (px.in_fd != -1)
+		close(px.in_fd);
+	close(outfile_fd);
 	waitpid(px.pid1, NULL, 0);
 	waitpid(px.pid2, &status, 0);
 	if (WIFEXITED(status))
