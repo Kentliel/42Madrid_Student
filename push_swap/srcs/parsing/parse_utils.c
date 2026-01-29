11ae826c6f52d8c59de4a8e73c201b1fc1c16a74
Kentliel <kent.iluminati@gmail.com>
2026-01-29 20:23:55 +0100

    creacion de la funcion ults para el parseo otra vez

---
+++ push_swap/srcs/parsing/parse_utils.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/29 20:19:39 by kcarrero          #+#    #+#             */
+/*   Updated: 2026/01/29 20:23:04 by kcarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str || str[0] == '\0')
+		return (0);
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!ft_isdigit(str[i]))
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+
