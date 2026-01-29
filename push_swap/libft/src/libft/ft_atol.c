ce8ad7975eaf3fbc3281c3304e3888b6056ac5f4
Kentliel <kent.iluminati@gmail.com>
2026-01-29 23:04:41 +0100

    mejora de la funcion atol

--- push_swap/libft/src/libft/ft_atol.c
+++ push_swap/libft/src/libft/ft_atol.c
@@ -6,39 +6,63 @@
 /*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 20:30:19 by kcarrero          #+#    #+#             */
-/*   Updated: 2026/01/29 20:43:00 by kcarrero         ###   ########.fr       */
+/*   Updated: 2026/01/29 23:03:24 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	skip_spaces(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	return (i);
+}
+
+static void	set_sign(const char *str, int *i, long *sign)
+{
+	if (str[*i] == '+' || str[*i] == '-')
+	{
+		if (str[*i] == '-')
+			*sign = -1;
+		(*i)++;
+	}
+}
+
+static int	check_overflow(long result, int sign, int digit)
+{
+	if (result > (LONG_MAX - digit) / 10)
+	{
+		if (sign == 1)
+			return (1);
+		return (-1);
+	}
+	return (0);
+}
 
 long	ft_atol(const char *str)
 {
 	long	result;
 	long	sign;
 	int		i;
+	int		ov;
 
 	result = 0;
 	sign = 1;
-	i = 0;
-	while (ft_isspace(str[i]))
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
+	i = skip_spaces(str);
+	set_sign(str, &i, &sign);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (res > (LONG_MAX - (str[i] - '0')) / 10)
-		{
-			if (sign == 1)
-				return (LONG_MAX);
+		ov = check_overflow(result, sign, str[i] - '0');
+		if (ov == 1)
+			return (LONG_MAX);
+		if (ov == -1)
 			return (LONG_MIN);
-		}
 		res = res * 10 + (str[i] - '0');
 		i++;
 	}
-	return (res * sign);
+	return (result * sign);
 }
