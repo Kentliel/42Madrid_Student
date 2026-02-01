5e0a971d31bec115df1b20dc5bf3201a32611b5d
Kent Carrero <kcarrero@c1r15s3.42madrid.com>
2026-02-01 11:34:45 +0100

    mejora de la comprobacion de espacios al enviar la cadena de numeros

--- push_swap/srcs/parsing/parse_args.c
+++ push_swap/srcs/parsing/parse_args.c
@@ -6,7 +6,7 @@
 /*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 20:09:05 by kcarrero          #+#    #+#             */
-/*   Updated: 2026/01/30 00:59:10 by kcarrero         ###   ########.fr       */
+/*   Updated: 2026/01/30 13:36:13 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,24 @@ static void	add_number(t_stack *stack, char *str)
 	stack->size_a++;
 }
 
+static int	invalid_args(char *str)
+{
+	int	i;
+
+	if (!str || !str[0])
+		return (1);
+	if (ft_isspace(str[0]))
+		return (1);
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isspace(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	parse_args(int argc, char **argv, t_stack *stack)
 {
 	int		i;
@@ -38,13 +56,16 @@ void	parse_args(int argc, char **argv, t_stack *stack)
 	i = 1;
 	while (i < argc)
 	{
+		if (invalid_args(argv[i]))
+			error_exit(stack, "Error\n");
 		split = ft_split(argv[i], ' ');
 		if (!split)
 			error_exit(stack, "Error\n");
 		j = 0;
 		while (split[j])
 		{
-			add_number(stack, split[j]);
+			if (split[j])
+				add_number(stack, split[j]);
 			j++;
 		}
 		ft_free_split(split);
