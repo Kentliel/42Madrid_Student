31b71d16620f15004bba97e36556c04d04b5b4b3
Kentliel <kent.iluminati@gmail.com>
2026-02-03 12:29:34 +0100

    modularizacion de la funcion parse args por lineas

--- push_swap/srcs/parsing/parse_args.c
+++ push_swap/srcs/parsing/parse_args.c
@@ -6,27 +6,28 @@
 /*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 20:09:05 by kcarrero          #+#    #+#             */
-/*   Updated: 2026/01/30 13:36:13 by kcarrero         ###   ########.fr       */
+/*   Updated: 2026/02/03 12:27:13 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	add_number(t_stack *stack, char *str)
+static int	add_number(t_stack *stack, char *str)
 {
 	long	value;
 	t_node	*new;
 
 	if (!is_number(str))
-		error_exit(stack, "Error\n");
+		return (0);
 	value = ft_atol(str);
 	if (value > INT_MAX || value < INT_MIN)
-		error_exit(stack, "Error\n");
+		return (0);
 	new = stack_new((int)value);
 	if (!new)
-		error_exit(stack, "Error\n");
+		return (0);
 	stack_add_back(&stack->a, new);
 	stack->size_a++;
+	return (1);
 }
 
 static int	invalid_args(char *str)
@@ -47,28 +48,37 @@ static int	invalid_args(char *str)
 	return (1);
 }
 
+static void	parse_one_arg(t_stack *stack, char *arg)
+{
+	char	**split;
+	int		j;
+
+	if (invalid_args(arg))
+		error_exit(stack, "Error\n");
+	split = ft_split(arg, ' ');
+	if (!split)
+		error_exit(stack, "Error\n");
+	j = 0;
+	while (split[j])
+	{
+		if (!add_number(stack, "Error\n"))
+		{
+			ft_free_split(split);
+			error_exit(stack, "Error\n");
+		}
+		j++;
+	}
+	ft_free_split(split);
+}
+
 void	parse_args(int argc, char **argv, t_stack *stack)
 {
 	int		i;
-	char	**split;
-	int		j;
 
 	i = 1;
 	while (i < argc)
 	{
-		if (invalid_args(argv[i]))
-			error_exit(stack, "Error\n");
-		split = ft_split(argv[i], ' ');
-		if (!split)
-			error_exit(stack, "Error\n");
-		j = 0;
-		while (split[j])
-		{
-			if (split[j])
-				add_number(stack, split[j]);
-			j++;
-		}
-		ft_free_split(split);
+		parse_one_arg(stack, argv[i]);
 		i++;
 	}
 	if (is_duplicates(stack->a))
