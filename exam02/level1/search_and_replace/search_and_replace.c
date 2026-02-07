df7506cd27a9a0e55443881aa36c9f2341477567
Kentliel <kent.iluminati@gmail.com>
2026-02-07 15:49:26 +0100

    empezando hacer el ejercicio de buscar los 3 argumentos y cambiar el valor del primer string

---
+++ exam02/level1/search_and_replace/search_and_replace.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_and_replace.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/07 15:01:13 by kcarrero          #+#    #+#             */
+/*   Updated: 2026/02/07 15:48:50 by kcarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	char	c;
+
+	if (argc != 4)
+		return (write(1, "\n", 1));
+	if (argv[2][1] != '\0' || argv[3][1] != '\0')
+		return (write(1, "\n", 1));
+	i = 0;
+	while (argv[1][i])
+	{
+		c = argv[1][i];
+		if (c == argv[2][0])
+			c = argv[3][0];
+	}
+}
