845349fb5e3642e5a4c7c286c0f2254a80d5e40a
Kentliel <kent.iluminati@gmail.com>
2026-02-14 15:26:35 +0100

    ejercicio alpha mirror

--- exam02/level2/alpha_mirror/alpha_mirror.c
+++ exam02/level2/alpha_mirror/alpha_mirror.c
@@ -6,10 +6,30 @@
 /*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 15:12:24 by kcarrero          #+#    #+#             */
-/*   Updated: 2026/02/14 15:12:40 by kcarrero         ###   ########.fr       */
+/*   Updated: 2026/02/14 15:25:31 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+int	main(int argc, char **argv)
+{
+	int		i;
+	char	c;
 
+	if (argc != 2)
+		return (write(1, "\n", 1));
+	i = 0;
+	while (argv[1][i])
+	{
+		c = argv[1][i];
+		if (c >= 'a' && c <= 'z')
+			c = 'z' - (c - 'a');
+		else if (c >= 'A' && c <= 'Z')
+			c = 'Z' - (c - 'A');
+		write(1, &c, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
