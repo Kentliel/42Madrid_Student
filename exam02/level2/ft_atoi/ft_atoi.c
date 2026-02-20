1a2f1a0f2440522d63ced2b12d3146439efaafc9
Kentliel <kent.iluminati@gmail.com>
2026-02-20 15:13:08 +0100

    ft_atoi hecho

---
+++ exam02/level2/ft_atoi/ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/20 14:59:31 by kcarrero          #+#    #+#             */
+/*   Updated: 2026/02/20 15:12:38 by kcarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	signo;
+	int	result;
+
+	i = 0;
+	signo = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			signo = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * signo);
+}
