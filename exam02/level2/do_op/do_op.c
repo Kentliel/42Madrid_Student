9bf44ee8b76015e0208b2040ac95073bf799bab0
Kentliel <kent.iluminati@gmail.com>
2026-02-20 14:54:06 +0100

    do_op hecho

--- exam02/level2/do_op/do_op.c
+++ exam02/level2/do_op/do_op.c
@@ -6,9 +6,32 @@
 /*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 15:54:04 by kcarrero          #+#    #+#             */
-/*   Updated: 2026/02/14 15:59:33 by kcarrero         ###   ########.fr       */
+/*   Updated: 2026/02/20 14:53:11 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+int	main(int argc, char **argv)
+{
+	int	a;
+	int	b;
+
+	if (argc != 4)
+		return (write(1, "\n", 1));
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (argv[2][0] == '+' )
+		return (printf("%d\n", a + b));
+	else if (argv[2][0] == '-')
+		return (printf("%d\n", a - b));
+	else if (argv[2][0] == '*')
+		return (printf("%d\n", a * b));
+	else if (argv[2][0] == '/')
+		return (printf("%d\n", a / b));
+	else if (argv[2][0] == '%')
+		return (printf("%d\n", a % b));
+	return (0);
+}
