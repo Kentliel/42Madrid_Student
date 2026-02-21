8d2167bf63728be8b1bbda6e047c42ba9fdd173b
Kentliel <kent.iluminati@gmail.com>
2026-02-21 14:29:33 +0100

    creacion del ft_strcspn

---
+++ exam02/level2/ft_strcspn/ft_strcspn.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcspn.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/21 14:19:52 by kcarrero          #+#    #+#             */
+/*   Updated: 2026/02/21 14:28:17 by kcarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (reject[j])
+		{
+			if (s[i] == reject[j])
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
