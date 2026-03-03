ed86df59457b886dfa6f3735ce25d9dd6b6b9355
Kentliel <kent.iluminati@gmail.com>
2026-03-03 15:45:26 +0100

    creacion del itoa

---
+++ exam02/level4/ft_itoa/ft_itoa.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/03 15:39:15 by kcarrero          #+#    #+#             */
+/*   Updated: 2026/03/03 15:45:11 by kcarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	count_len(long n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+	{
+		len++;
+		n = -n;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int nbr)
+{
+	long	n;
+	int		len;
+	char	*str;
+
+	n = nbr;
+	len = count_len(n);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -n;
+	}
+	while (n > 0)
+	{
+		str[--len] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
