6133ea35f09682ad7d874377b3b379ccabae7d48
Kentliel <kent.iluminati@gmail.com>
2026-03-04 23:32:49 +0100

    creacion de rstr_capitalizer

---
+++ exam02/level3/rstr_capitalizer/rstr_capitalizer.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rstr_capitalizer.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 23:26:10 by kcarrero          #+#    #+#             */
+/*   Updated: 2026/03/04 23:32:29 by kcarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	is_letter(char c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+char	to_lower(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		c += 32;
+	return (c);
+}
+
+char	to_upper(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		c -= 32;
+	return (c);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	c;
+
+	if (argc < 2)
+		return (write(1, "\n", 1), 0);
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			c = to_lower(argv[i][j]);
+			if (is_letter(c) && !is_letter(argv[i][j + 1]))
+				c = to_upper(c);
+			write(1, &c, 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
