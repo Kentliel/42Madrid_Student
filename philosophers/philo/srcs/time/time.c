308952a0d74f1a7d0f2ad1eb7041aefca0b6a8d4
Kent Carrero <kent.iluminati@gmail.com>
2026-04-27 15:17:05 +0200

    creacion de los comentarios de las funciones de time

--- philosophers/philo/srcs/time/time.c
+++ philosophers/philo/srcs/time/time.c
@@ -6,12 +6,13 @@
 /*   By: kcarrero <kcarrero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/12 15:08:11 by kcarrero          #+#    #+#             */
-/*   Updated: 2026/04/12 15:18:50 by kcarrero         ###   ########.fr       */
+/*   Updated: 2026/04/27 15:16:27 by kcarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+/*Devuelve el tiempo actual en milisegundos*/
 long	get_time_ms(void)
 {
 	struct timeval	tv;
@@ -20,11 +21,14 @@ long	get_time_ms(void)
 	return ((tv.tv_sec * 1000L) + (tv.tv_usec / 1000L));
 }
 
+/*Devuelve los milisegundos transcurridos desde start_time*/
 long	timestamp_ms(long start_time)
 {
 	return (get_time_ms() - start_time);
 }
 
+/*Duerme de forma precisa durante duration_ms o hasta que termine la simulacion
+ *comprueba periodicamente simulation_finished para poder interrumpir el sueño*/
 void	precise_sleep(t_table *table, long duration_ms)
 {
 	long	start;
